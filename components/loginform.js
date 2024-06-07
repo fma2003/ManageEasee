a7f6b95d34aa439d8219f6d6e39b83e462984fcf
fma2003 <fma2003@hw.ac.uk>
2024-06-07 15:21:59 +0400

    login page complete

--- components/loginform.js
+++ components/loginform.js
@@ -1,6 +1,29 @@
+'use  client'
 import PropTypes from "prop-types";
+import { useState } from "react";
+import {useSignInWithEmailAndPassword} from  'react-firebase-hooks/auth'
+import {auth} from '../utile/firebase/config'
+import {useRouter} from 'next/navigation'
 
-const Credentials = ({ className = "" }) => {
+const Login = () => {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [signInWithEmailAndPassword] = useSignInWithEmailAndPassword(auth); 
+  const router = useRouter()
+
+  const handleLogin = async () => {
+    try{
+      const res = await signInWithEmailAndPassword(email, password)
+      console.log({res})  
+      setEmail('');
+      setPassword('');
+      router.push('/')
+    } catch(e){
+      console.error(e)
+    }
+  };
+
   return (
     <div
       className={"relative w-[400px] h-[598px] text-left text-29xl text-white font-k2d lg:w-[445px] sm:align-left sm:w-[1200px] md:left-2 ${className}"}
@@ -17,13 +40,16 @@ const Credentials = ({ className = "" }) => {
       <input
         className="[outline:none] bg-black absolute top-[235px] left-[66px] rounded-xl box-border w-[328px] xs:w-[300px] h-[77px] border-[1px] border-solid border-white text-white text-5xl"
         placeholder="Enter Email"
-        type="text"
-        form="Email"
+        type="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
       />
       <input
         className="[outline:none] bg-black absolute top-[337px] left-[66px] rounded-xl box-border w-[328px] xs:w-[300px] h-[77px] border-[1px] border-solid border-white text-white text-5xl"
         placeholder="Enter Password"
         type="text"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
       />
       <button className="cursor-pointer [border:none] p-0 bg-[transparent] absolute top-[427px] left-[67px] text-2xl font-k2d text-skyblue text-left inline-block w-[249px] h-[23px]">
         <p className="m-0">Forgot your password?</p>
@@ -35,7 +61,9 @@ const Credentials = ({ className = "" }) => {
       <div className="absolute top-[539px] left-[calc(50%_-_49px)] text-5xl text-black inline-block w-[97px] h-[34px]">
         Continue
       </div>
-      <button className="cursor-pointer [border:none] p-0 bg-[transparent] absolute top-[538px] left-[67px] w-[325px] h-[52px] transform transition-transform duration-300 hover:scale-105 active:scale-95 ">
+      <button
+        onClick={handleLogin}
+        className="cursor-pointer [border:none] p-0 bg-[transparent] absolute top-[538px] left-[67px] w-[325px] h-[52px] transform transition-transform duration-300 hover:scale-105 active:scale-95 ">
         <div className="absolute h-full w-full top-[0%] right-[0%] bottom-[0%] left-[0%] rounded-mini bg-gainsboro" />
         <div className="absolute h-[65.38%] w-[38.86%] top-[17.31%] left-[36.34%] text-5xl font-semibold font-k2d text-black text-left inline-block">
           Continue
@@ -49,8 +77,8 @@ const Credentials = ({ className = "" }) => {
   );
 };
 
-Credentials.propTypes = {
+Login.propTypes = {
   className: PropTypes.string,
 };
 
-export default Credentials;
+export default Login;
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,7 @@
-import Head from "next/head";
-import Loginform from "../components/loginform";
+import Head from 'next/head';
 
-const Desktop = () => {
-  return (
-    <main className="w-full relative bg-black h-[1060px] sm:w-[420px]">
-      <div className="absolute top-[calc(50%_-_512px)] left-[0px] w-[1432px] h-[1024px] flex flex-col items-start justify-between mix-blend-screen lg:w-[620px] lg:items-start lg:justify-center md:hidden">
-        <video
-          className="w-[1432px] h-[1040px] relative  rounded-tl-none rounded-tr-70xl  rounded-bl-none mix-blend-screen object-cover lg:h-[1024px] lg:object-cover md:hidden md:self-stretch md:w-auto md:flex-1 sm:hidden"
-          autoPlay
-          muted
-          loop
-        >
-          <source src="/aura_(2).mp4" />
-        </video>
-      </div>
-      <div className="absolute top-[calc(50%_-_299px)] left-[calc(50%_-_234px)] overflow-hidden flex flex-row flex-wrap items-center justify-center py-0 pr-0 pl-6 lg:w-[1010px] lg:items-center lg:justify-end lg:pl-0 lg:pr-[-500-] lg:box-border md:w-auto md:[align-self:unset] md:flex-row md:flex-wrap md:items-center md:justify-start md:pl-0 md:pr-0 md:box-border md:[align-content:safe_center] sm:w-[470px] sm:items-center sm:justify-center sm:pl-0 sm:pr-[150px] sm:box-border">
-        <Loginform />
-      </div>
-    </main>
-  );
-};
-
-export default Desktop;
+export default function Home() {
+    return (
+        <h1>Hello World</h1>
+    );
+}
